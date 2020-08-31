122fd9953e46b4cd9b650089d354ca3d59a68fb2
stallinj6429 <70527339+stallinj6429@users.noreply.github.com>
2020-08-31 11:35:43 -0400

    Add files via upload
    Basic Math

---
+++ P1HW2_BasicMath_StallingsJoshua.py
@@ -0,0 +1,17 @@
+#Input and output wiht basic math
+#31AUG2020
+#CTI-110 P1HW2 - Basic Math
+#Stallings, Joshua
+#
+
+firstNumber = int(input("Please Enter first number: "))   #Enter number from user
+secondNumber = int(input("Please Enter second number: ")) #Enter number from user
+print("First number entered:", firstNumber)   #Display number entered
+print("Second number entered:", secondNumber) #Display number entered
+sumNumber = firstNumber + secondNumber #get the sum of the two numbers
+productNumber = firstNumber * secondNumber #get the product of the two numbers
+print("\nSum of both numbers:", sumNumber) #display the sum
+print("Result of Multiplying the two numbers:", productNumber) #display the product
+turtlegraphics().exitonclick()
+
+
