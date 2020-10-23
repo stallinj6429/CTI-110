77177290bfc550ce408096976055376a8a355bd5
stallinj6429 <70527339+stallinj6429@users.noreply.github.com>
2020-10-23 09:22:50 -0400

    Track Laps

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-current_price = int(input())
-last_months_price = int(input())
-differenceInPrice = current_price - last_months_price
-mortgage = (current_price * 0.051) / 12
-''' Type your code here. '''
-print("This house is $",current_price,". The change is $",differenceInPrice,"since last month.")
-print("The estimated monthly mortgage is $",'{:.2f}'.format(mortgage),'.')
+def miles_to_laps(miles):
+    laps = miles * 4
+    return laps
+
+if __name__ == '__main__':
+    userMiles = float(input())
+    print('{:.2f}'.format(miles_to_laps(userMiles)))
